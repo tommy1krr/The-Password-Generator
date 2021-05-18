712e1557ab3dd1c32f4dc03d7d315f858aac23be
tommy1krr <81730071+tommy1krr@users.noreply.github.com>
2021-05-18 18:22:10 -0500

    first commit

---
+++ Develop/script.js
@@ -0,0 +1,30 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+
+if generate password is clicked 
+then generate password
+
+generate password is Random
+choose capital-letters
+choose lowcase-letters
+choose numbers
+choose special characters
+choose the number of characters in password
+    var password_length = number;
+
+how to randomize the character locations
+
+
