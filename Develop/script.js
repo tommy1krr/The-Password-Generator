041c316d8c7ac96060145ace5458f363588eb210
tommy1krr <81730071+tommy1krr@users.noreply.github.com>
2021-05-21 00:45:19 -0500

    added radios

--- Develop/script.js
+++ Develop/script.js
@@ -1,18 +1,39 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var capital = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+var lower = "abcdefghijklmnopqrstuvwxyz".split("");
+var number = "0123456789".split("");
+var special = "!#$%&()*+-/=>@[]^_{}|~".split("");
+
+
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
+}
+
+// Add event listener to ok button
+function charchoice(event){
+  var 
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+function addTip(event) {
+  event.preventDefault();
+  var tipPercentage = tipEl.value * .01;
+  var total = totalEl.value;
+  var tipAmount = calculateTip(total, tipPercentage);
+  var newTotal = calculateTotal(tipAmount, total);
+  document.querySelector("#tip-amount").textContent = tipAmount;
+  document.querySelector("#new-total").textContent = newTotal.toFixed(2);
+}
 
+
+
+// Add event listener to generate button
+/* generateBtn.addEventListener("click", writePassword);
 
 if generate password is clicked 
 then generate password
@@ -24,7 +45,8 @@ choose numbers
 choose special characters
 choose the number of characters in password
     var password_length = number;
+    charArray []
 
 how to randomize the character locations
 
-
+ */
