d6cb03fdd12fbd45c008fd0dd16798929a551595
tommy1krr <81730071+tommy1krr@users.noreply.github.com>
2021-05-21 20:50:43 -0500

    added event listener and array

--- Develop/script.js
+++ Develop/script.js
@@ -1,52 +1,57 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var capital = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
-var lower = "abcdefghijklmnopqrstuvwxyz".split("");
-var number = "0123456789".split("");
-var special = "!#$%&()*+-/=>@[]^_{}|~".split("");
+var capitalEl = document.querySelector("#capital");
+var lowerEl = document.querySelector("#lower");
+var numbersEl = document.querySelector("#numbers");
+var specialEl = document.querySelector("#special");
+var passwordEl = document.querySelector("#password");
+var lengthEl = document.querySelector("#length");
 
 
 
-// Write password to the #password input
+// Write password to the password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
 }
 
-// Add event listener to ok button
-function charchoice(event){
-  var 
-
-}
-
-function addTip(event) {
-  event.preventDefault();
-  var tipPercentage = tipEl.value * .01;
-  var total = totalEl.value;
-  var tipAmount = calculateTip(total, tipPercentage);
-  var newTotal = calculateTotal(tipAmount, total);
-  document.querySelector("#tip-amount").textContent = tipAmount;
-  document.querySelector("#new-total").textContent = newTotal.toFixed(2);
-}
-
-
 
 // Add event listener to generate button
-/* generateBtn.addEventListener("click", writePassword);
+generate.addEventListener("click", () => {
+  const length = +lengthEl.value;
+  const CapitalBox = capitalEl.checked;
+  const LowerBox = lowerEl.checked;
+  const NumbersBox = numbersEl.checked;
+  const SpecialBox = specialEl.checked;
 
-if generate password is clicked 
-then generate password
+  passwordEl.innerText = writePassword;
+});
 
-generate password is Random
-choose capital-letters
-choose lowcase-letters
-choose numbers
-choose special characters
-choose the number of characters in password
-    var password_length = number;
-    charArray []
 
-how to randomize the character locations
+function geratePassword (capitalEl, lowerEl, numberEl, specialEl, lengthEl) {
+  let generatePassword = "";
+  const typesCount = capitalEl + lowerEl + numberEl + specialEl;
+  const typesArr = [{capitalEl}, {lowerEl}, {numberEl}, {specialEl}].filter(item =>
+    Object.values(item)[0]);
+    if(typesCount === 0){
+      return '';
+    }
+}
 
- */
+
+
+// if generate password is clicked 
+// then generate password
+
+// generate password is Random
+// choose capital-letters
+// choose lowcase-letters
+// choose numbers
+// choose special characters
+// choose the number of characters in password
+//     var password_length = number;
+//     charArray []
+
+// how to randomize the character locations
+
--- README.md
+++ README.md
@@ -5,7 +5,13 @@ Thomas Kang Password Generator
 -  formulated the structure of javascript code
 
 
-5/21/2021
+5/20/2021
 -  added radio buttons to select the character types to be generated in the password
 -  added the pasword length box
 -  added the var for the character types
+
+5/21/2021
+-  added event listener
+-  updated global variables
+-  added length variable
+-  types array for capital, lower, numbers, special
